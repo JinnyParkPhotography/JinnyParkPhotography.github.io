79bcdba409ac0a49d9a975a64d774b56d88e9574
JinnyParkPhotography <112672186+JinnyParkPhotography@users.noreply.github.com>
2024-08-13 09:16:43 +0900

    IMAGE_LIST Update

--- src/component/page/Gallery.js
+++ src/component/page/Gallery.js
@@ -1,7 +1,7 @@
 class Gallery extends Component{
     setup() {
-      const imageList = this.getImageList();
-      this.state = { imageList };
+        alert(IMAGE_LIST);
+      //this.state = { imageList };
     }
     template() {
       const { imageList } = this.state;
@@ -9,16 +9,5 @@ class Gallery extends Component{
         <h2>Gallery This is My Gallery</h2>
         ${imageList.map(v => `<li>${v.id}</li>`)}
       `;
-    }
-    async getImageList() {
-      const response = await fetch('https://picsum.photos/v2/list');
-      if(response.ok) {
-        const json = await response.json();
-        alert("From getImageList: "+response);
-        return json;
-      } else {
-        alert("HTTP-Error: " + response.status);
-        return;
-      }
     }
   }

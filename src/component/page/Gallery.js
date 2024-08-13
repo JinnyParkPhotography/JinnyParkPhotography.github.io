a574eee9f20f42beb1226ac0e365084920ba9af1
JinnyParkPhotography <112672186+JinnyParkPhotography@users.noreply.github.com>
2024-08-13 09:23:02 +0900

    IMAGE_LIST lifting upside

--- src/component/page/Gallery.js
+++ src/component/page/Gallery.js
@@ -1,13 +1,12 @@
 class Gallery extends Component{
     setup() {
         alert(IMAGE_LIST);
-      //this.state = { imageList };
     }
-    template() {
-      const { imageList } = this.state;
-      return `
-        <h2>Gallery This is My Gallery</h2>
-        ${imageList.map(v => `<li>${v.id}</li>`)}
-      `;
-    }
+    // template() {
+    //   const { imageList } = this.state;
+    //   return `
+    //     <h2>Gallery This is My Gallery</h2>
+    //     ${imageList.map(v => `<li>${v.id}</li>`)}
+    //   `;
+    //}
   }

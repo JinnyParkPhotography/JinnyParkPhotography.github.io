8ef66c8be8ef94b2f54f1d7adb106a1f4e331c1d
JinnyParkPhotography <112672186+JinnyParkPhotography@users.noreply.github.com>
2024-08-13 06:28:01 +0000

    main layout max-width set 100%

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,7 +1,8 @@
 class Gallery extends Component{
     setup() {
       this.state = {
-        imageList: IMAGE_LIST
+        imageList: IMAGE_LIST,
+        selectedId: ''
       }
     }
     mounted() {
@@ -10,15 +11,21 @@ class Gallery extends Component{
         const $galleryItem = this.$target.querySelector(`[data-component="thumbnail-${image.id}"]`);
         new Thumbnail($galleryItem, image);
       });
+      if(selectedId !== '') {
+        const $modal = this.$target.querySelector('[data-component="modal"]');
+        const selectedImage = imageList.find(image => image.id === selectedId);
+        new Modal($modal, selectedImage);
+      }
     }
     template() {
       const { imageList } = this.state;
       return `
-        <div style="display: flex;">
+        <div style="display: flex; flex-wrap: wrap; justify-content: center;">
           ${imageList.map(image => `
               <div data-component="thumbnail-${image.id}"></div>
           `).join('')}
         </div>
+        <div data-component="modal" style="display: absolute; width: 100%; height: 100%"></div>
       `;
     }
   }

2187086bf3ab85e09c8ad2f4d00be21bc4669704
JinnyParkPhotography <112672186+JinnyParkPhotography@users.noreply.github.com>
2024-08-13 08:17:52 +0000

    Separate Modal or Thumbnail

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -7,33 +7,35 @@ class Gallery extends Component{
     }
     mounted() {
       const { imageList, selectedId } = this.state;
-      imageList.forEach(image => {
-        const { id, author, width, height, download_url } = image;
-        const $galleryItem = this.$target.querySelector(`[data-component="thumbnail-${id}"]`);
-        new Thumbnail($galleryItem, {
-          id, author, width, height, download_url,
-          selectIdHandler: this.selectIdHandler.bind(this)
-        });
-      });
       if(selectedId) {
         const selectedImage = imageList.find(image => image.id === selectedId);
         const { id, author, width, height, download_url } = selectedImage;
         const $modal = this.$target.querySelector('[data-component="modal"]');
         new Modal($modal, { id, author, width, height, download_url });
+      } else {
+        imageList.forEach(image => {
+          const { id, author, width, height, download_url } = image;
+          const $galleryItem = this.$target.querySelector(`[data-component="thumbnail-${id}"]`);
+          new Thumbnail($galleryItem, {
+            id, author, width, height, download_url,
+            selectIdHandler: this.selectIdHandler.bind(this)
+          });
+        });
       }
     }
     template() {
       const { imageList, selectedId } = this.state;
       return `
-        <div style="display: flex; flex-wrap: wrap; justify-content: center;">
-          ${imageList.map(image => `
-              <div data-component="thumbnail-${image.id}"></div>
-          `).join('')}
-        </div>
         ${selectedId ? `
           <div data-component="modal" 
             style="display: absolute; width: 90%; height: 90%"></div>  
-        ` : ''}
+        ` : `
+          <div style="display: flex; flex-wrap: wrap; justify-content: center;">
+            ${imageList.map(image => `
+                <div data-component="thumbnail-${image.id}"></div>
+            `).join('')}
+          </div>
+        `}
       `;
     }
     selectIdHandler(selectedId) {
